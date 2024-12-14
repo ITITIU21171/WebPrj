abbc56acc51cb0ef47c52d19aaf56ceba418128a
ITITIU21171 <118947331+ITITIU21171@users.noreply.github.com>
2024-12-14 23:29:45 +0700

    add image to dtb
    them anh?

--- nodejs/connectDB.js
+++ nodejs/connectDB.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost', 
   user: 'root',      
-  password: '12345',
+  password: 'root',
   database: 'car_selling_nodejs', 
   multipleStatements: true 
 });
